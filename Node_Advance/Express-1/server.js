f7a7e1372252e5a79587a09c183afe78237a57b6
udit22022000 <ping2udit@gmail.com>
2021-09-16 21:00:03 +0530

    updated express 1

--- Node_Advance/Express-1/server.js
+++ Node_Advance/Express-1/server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 const connect = () => {
-  return mongoose.connect("mongodb://127.0.0.1:27017", {
+  return mongoose.connect("mongodb://127.0.0.1:27017/TestDB", {
     useCreateIndex: true,
     useFindAndModify: false,
     useNewUrlParser: true,
@@ -37,6 +37,37 @@ const userSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model("User", userSchema); //make sure database name is in singular...
+
+app.use(express.json());
+
+app.get("/users", async (req, res) => {
+  const users = await User.find().lean().exec();
+  res.send(users);
+});
+
+app.get("/user/:id", async (req, res) => {
+  const users = await User.find({ _id: req.params.id });
+
+  res.send(users);
+});
+
+app.post("/users", async (req, res) => {
+  const user = await User.create(req.body);
+
+  res.status(201).send(user);
+});
+
+app.patch("/user/:id", async (req, res) => {
+  const user = await User.updateOne({ _id: req.params.id }, { $set: req.body });
+
+  res.send(user);
+});
+
+app.delete("/user/:id", async (req, res) => {
+  const user = await User.deleteOne({ _id: req.params.id });
+
+  res.send(user);
+});
 
 app.listen(3050, async () => {
   await connect();
